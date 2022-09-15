0d2d2394a3be866bb3191de481424d042a71e644
Ankit Kumar singh <70365778+Ankitsingh332@users.noreply.github.com>
2022-09-15 12:54:32 +0530

    Add files via upload

---
+++ PracticeProject1/PracticeProject1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PracticeProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = File.ReadAllText("D:\\Simplilearn Practice_Project\\StudentData.txt");
+            Console.WriteLine("Data From the File display here-");
+            Console.WriteLine("=========================================");
+            Console.WriteLine(data);
+            Console.WriteLine("=========================================");
+
+            Console.ReadLine();
+        }
+    }
+}
